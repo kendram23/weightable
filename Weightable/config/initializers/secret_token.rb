aeabfcb60959ede5e3b35b2cb8d95beaf8fc2ff2
MakerSquare Student <students@themakersquare.com>
2013-12-20 16:41:39 -0600

    first commit

---
+++ Weightable/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weightable::Application.config.secret_key_base = '473ca23d1e4150fe6a987240635bbd7be2f4276a1968439c06ab8b2bfa24bc83b7720ae100d63e41e78e0abc90d1974a68bd352738dad69fef05cc5318c691b5'
