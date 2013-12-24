8c43d0a6736688883bf701ffb8946057e92c6e52
MakerSquare Student <students@themakersquare.com>
2013-12-24 11:09:59 -0600

    Add about to users

--- Weightable/db/schema.rb
+++ Weightable/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131224161806) do
+ActiveRecord::Schema.define(version: 20131224165512) do
 
   create_table "friendships", force: true do |t|
     t.integer "friendable_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20131224161806) do
     t.integer  "goal_weight"
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "about"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
@@ -56,9 +57,11 @@ ActiveRecord::Schema.define(version: 20131224161806) do
     t.integer  "current_weight"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id_id"
     t.integer  "user_id"
   end
 
   add_index "weigh_ins", ["user_id"], name: "index_weigh_ins_on_user_id"
+  add_index "weigh_ins", ["user_id_id"], name: "index_weigh_ins_on_user_id_id"
 
 end
