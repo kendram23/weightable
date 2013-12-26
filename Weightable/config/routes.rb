e432c9b81275370282e89795f94657715a01ec16
MakerSquare Student <students@themakersquare.com>
2013-12-26 17:15:17 -0600

    Add user index page

--- Weightable/config/routes.rb
+++ Weightable/config/routes.rb
@@ -1,6 +1,7 @@
 Weightable::Application.routes.draw do
   devise_for :users
 
-  get 'users/:id' => 'users#show', as: 'user'
+  get 'users/:id' => 'users#show',  as: 'user'
+  get 'users'     => 'users#index', as: "users"
 
 end
