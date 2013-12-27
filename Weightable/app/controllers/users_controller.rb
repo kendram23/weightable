447b42d51f3d9b9812ba9100ce263d98995e8709
MakerSquare Student <students@themakersquare.com>
2013-12-27 11:56:07 -0600

    Add callbacks to user controller

--- Weightable/app/controllers/users_controller.rb
+++ Weightable/app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  
 def show
     @user = User.find(params[:id])
 end
@@ -9,9 +10,11 @@ def index
 	@users = User.paginate(:page => params[:page], :per_page => 20)
 end
 
+  private
 
-def test
-
-end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
   
 end
