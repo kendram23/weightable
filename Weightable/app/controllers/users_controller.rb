44c8043b7de41c0b5bc06f72753c2b013ce49220
MakerSquare Student <students@themakersquare.com>
2013-12-29 13:18:36 -0600

    Add sessions controller to restrict user edit

--- Weightable/app/controllers/users_controller.rb
+++ Weightable/app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -10,8 +11,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @users = User.find(params[:id])
-
+    correct_user = User.find(params[:id])
   end
 
   def update
@@ -29,6 +29,13 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :about, :start_weight, :goal_weight, :user_image)
+    end
+
+    # Before filters
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(user_path) unless current_user.id == @user.id
     end
 
 end
