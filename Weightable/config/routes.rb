255b8b47691a5a64eade1cc22b452bca129f10b0
MakerSquare Student <students@themakersquare.com>
2013-12-28 18:22:45 -0600

    Add home page for signin and authenticated root for home page

--- Weightable/config/routes.rb
+++ Weightable/config/routes.rb
@@ -3,5 +3,13 @@ Weightable::Application.routes.draw do
 
   resources :users, only: [:show, :index, :edit, :update]
   resources :weigh_ins, only: [:new, :create, :show]
+
+  authenticated :user do
+    root to: 'users#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root to: 'layouts#home'
+  end
   
 end
