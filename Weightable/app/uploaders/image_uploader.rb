921a8248827d9fbb15b2c4faf037e5d5437565d9
MakerSquare Student <students@themakersquare.com>
2013-12-30 22:45:05 -0600

    Add additional styling to user page

--- Weightable/app/uploaders/image_uploader.rb
+++ Weightable/app/uploaders/image_uploader.rb
@@ -42,6 +42,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fit => [250, 300]
   end
 
+    version :small do
+    process :resize_to_fit => [100, 100]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
